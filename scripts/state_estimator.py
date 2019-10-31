5bb1a66e0331699bb782326c34c9b4f08ed82c2f
mohradza <mioh0887@colorado.edu>
2019-10-31 13:05:53 -0600

    first commit

--- scripts/state_estimator.py
+++ scripts/state_estimator.py
@@ -3,9 +3,12 @@
 import rospy
 from sensor_msgs.msg import Range
 from z_state_estimator.msg import ZStateEst
-from rosflight_msgs.msg import Attitude
+#from rosflight_msgs.msg import Attitude
 from std_msgs.msg import Time
-from geometry_msgs.msg import Vector3Stamped
+from geometry_msgs.msg import Quaternion, TwistWithCovarianceStamped, Vector3Stamped
+from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
+import tf
 import math
 import numpy as np
 
@@ -16,15 +19,21 @@ class zStateEstimator:
         self.range_sub = rospy.Subscriber('/teraranger_evo', Range, self.range_cb, queue_size=10)
         self.attitude_sub = rospy.Subscriber('/attitude/euler', Vector3Stamped, self.attitude_cb, queue_size=10)
 
+        self.imu_sub = rospy.Subscriber('/imu_raw', Imu, self.imu_cb, queue_size=100)
+
         # Define publishers
         self.z_state_pub = rospy.Publisher('/z_state_estimator/z_state_estimate', ZStateEst, queue_size=10)
-        
+        self.z_twist_pub = rospy.Publisher('/z_state_estimator/twist', TwistWithCovarianceStamped, queue_size=100)
+        #self.z_twist_pub = rospy.Publisher('/z_state_estimator/odom', Odometry, queue_size=100)
+
         # Import Parameters
         self.h_filt_alpha = rospy.get_param('h_filt_alpha', .1)
         self.v_filt_alpha = rospy.get_param('v_filt_alpha', .1)
         self.range_variance = rospy.get_param('range_variance', .1)
-       
+
         # Initialize atittude
+        self.use_attitude = False
+        self.use_imu = True
         self.roll = 0.0
         self.pitch = 0.0
         self.tworot = np.identity(3)
@@ -40,6 +49,9 @@ class zStateEstimator:
         self.v_filt_last = 0.0
 
         self.est_msg = ZStateEst()
+        self.twist_msg = TwistWithCovarianceStamped()
+        self.twist_msg.header.frame_id = "z_est_frame"
+        self.twist_msg.twist.covariance[14] = .01
 
         # Kalman Filter Matrices / Vectors
         self.P = np.full((3,3),0)
@@ -62,56 +74,69 @@ class zStateEstimator:
         range_time_s = range_time.to_sec()
         dt = range_time_s - self.range_time_s_last
         self.range_time_s_last = range_time_s
-        
+
         # Convert range_val to h_agl using roll / pitch values
         h_agl_vec = self.onerot*self.tworot*np.matrix([[0],[0],[range_val]])
         #h_agl = h_agl_vec.item(2)
         h_agl = range_val*math.cos(self.roll)*math.cos(self.pitch)
-        print(h_agl)
+        #print(h_agl)
 
         # Low pass filter the h_agl value
         # to remove some of the sensor noise.
         # This improves the velocity estimates greatly.
         self.h_filt = (1-self.h_filt_alpha)*self.h_filt_last + self.h_filt_alpha*h_agl
         self.h_filt_last = self.h_filt
- 
+
         A = np.matrix([[1, 0, dt],[1, 0, 0],[1/dt, -1/dt, 0]])
         H = np.matrix([1, 0, dt])
-        
+
         if(not self.h_init):
             self.h_init = True
             self.xk = np.matrix([[self.h_filt], [self.h_filt], [0.0]])
             self.P = np.full((3,3),0)
-        
+
         # Predict Ahead
         xkp1 = A*self.xk
-        Pkp1 = A*self.P*A.transpose() + self.Q; 
-        
+        Pkp1 = A*self.P*A.transpose() + self.Q;
+
         # Compute Kalman Gain
         K = Pkp1*H.transpose()*np.linalg.inv(H*Pkp1*H.transpose() + self.R)
-        
+
         # Update State Estimate
         self.xk = xkp1 + K*(self.h_filt - H*xkp1)
-        
+
         # Update Error Covariance
         self.P = (np.identity(3) - K*H)*Pkp1
 
         # Filter the vertical velocity estimate
         self.v_filt = (1-self.v_filt_alpha)*self.v_filt_last + self.v_filt_alpha*self.xk.item(2)
         self.v_filt_last = self.v_filt
- 
+
         # Publish the estimate
         self.est_msg.header.stamp = rospy.Time.now()
         self.est_msg.height_agl.data = self.xk.item(0)
         self.est_msg.z_velocity.data = self.v_filt
         self.z_state_pub.publish(self.est_msg)
 
+        self.twist_msg.header.stamp = rospy.Time.now()
+        self.twist_msg.twist.twist.linear.z = self.v_filt
+        self.z_twist_pub.publish(self.twist_msg)
+
 
     def attitude_cb(self, msg):
-        self.roll = msg.vector.x
-        self.pitch = msg.vector.y
+        if(self.use_attitude):
+            self.roll = msg.vector.x
+            self.pitch = msg.vector.y
         #self.tworot = np.matrix([[math.cos(pitch), 0, -sin(pitch)],[0, 1, 0],[sin(pitch), 0, cos(pitch)]])
         #self.onerot = np.matrix([[1, 0, 0],[0, cos(roll), sin(roll)],[0, -sin(roll), cos(roll)]])
+
+    def imu_cb(self, msg):
+        if(self.use_imu):
+            imu_quat = (msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w)
+            euler = tf.transformations.euler_from_quaternion(imu_quat)
+            self.roll = euler[0]
+            self.pitch = euler[1]
+            #rospy.loginfo('Roll: %f, Pitch: %f', self.roll, self.pitch)
 
 if __name__ == '__main__':
     rospy.init_node('z_state_estimator')
@@ -120,7 +145,3 @@ if __name__ == '__main__':
     except:
         rospy.ROSInterruptException
     pass
-
-
-
-
